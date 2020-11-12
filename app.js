1fc6c1f875fd2e9c69888ab1e57f740944681133
小胡xiaohu <15111823732@163.com>
2020-11-12 16:14:07 +0800

    u

---
+++ app.js
@@ -0,0 +1,18 @@
+const nodeMediaServer = require("node-media-server")
+
+const config = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 60,
+        ping_timeout: 30
+    },
+    http: {
+        port: 8000,
+        allow_origin: '*'
+    }
+}
+
+var nms = new nodeMediaServer(config)
+nms.run();
