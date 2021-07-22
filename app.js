c630be474ed4e1e997123ec752a2a0e40d98e783
小胡xiaohu <15111823732@163.com>
2021-07-22 10:20:00 +0800

    u

--- app.js
+++ app.js
@@ -11,6 +11,23 @@ const config = {
     http: {
         port: 8000,
         allow_origin: '*'
+    },
+    relay: {
+        ffmpeg: 'D:\\哔哩哔哩压制工具\\tools\\ffmpeg.exe',
+        tasks: [
+            {
+                app: 'live',
+                mode: 'pull',
+                edge: 'rtsp://admin:wewin123456@172.16.100.236:554/h264/ch1/main/av_stream',
+                // edge: 'rtsp://admin:wewin123456@172.16.38.2:554/h264/ch1/main/av_stream',
+            },
+            {
+                app: 'live',
+                mode: 'pull',
+                // edge: 'rtsp://admin:wewin123456@172.16.100.236:554/h264/ch1/main/av_stream',
+                edge: 'rtsp://admin:wewin123456@172.16.38.2:554/h264/ch1/main/av_stream',
+            }
+        ]
     }
 }
 
